e0a88293e568a98546591b9f49b8c98b532bff1f
Mahdi Farmahini Farahani <aradfarahani@aol.com>
2024-08-15 01:04:12 +0330

    Create 330. Patching Array.py

---
+++ 330. Patching Array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minPatches(self, nums, n):
+        miss = 1
+        i = 0
+        patches = 0
+        
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patches += 1
+        
+        return patches
+
+# Example usage:
+solution = Solution()
+print(solution.minPatches([1, 3], 6))  # Output: 1
+print(solution.minPatches([1, 5, 10], 20))  # Output: 2
+print(solution.minPatches([1, 2, 2], 5))  # Output: 0

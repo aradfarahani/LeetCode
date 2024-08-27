287f78c0a98062499ea1734328a313f37e3901f1
Mahdi Farmahini Farahani <mahdifarmahini@khu.ac.ir>
2024-08-27 18:24:03 +0330

    Create 12. Integer to Roman.py

---
+++ 12. Integer to Roman.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+        
+        r = ''
+        
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            # If n in list then add the roman value to result variable
+            while n <= num:
+                r += num_map[n]
+                num-=n
+        return r

a2d68d59f358af78528caa3915f7be1b97a899cc
Mahdi Farmahini Farahani <mahdifarmahini@khu.ac.ir>
2024-08-27 15:34:31 +0330

    Create 5. Longest Palindromic Substring.py

---
+++ 5. Longest Palindromic Substring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        for i in range(len(s)-1):
+            for j in range(i+1,len(s)):
+                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
+                    Max_Len = j-i+1
+                    Max_Str = s[i:j+1]
+
+        return Max_Str
